8518012240ca2554410922d25c0eba86ec4793e0
Hamdi <TLiLiOS@yahoo.com>
2024-11-20 19:06:28 +0100

    Initial Commit

---
+++ GitHubProfilUser/GitHubProfilUserApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubProfilUserApp.swift
+//  GitHubProfilUser
+//
+//  Created by TLiLi Hamdi on 20/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct GitHubProfilUserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
